b403939d9464e6c8a3d0094ce2f414eefbb8db09
gdpoa1 <gdpoa1@iscte-iul.pt>
2025-06-01 00:32:18 +0100

    refactor: improve the structure of "main"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,18 +7,19 @@ import org.antlr.v4.runtime.CommonTokenStream
 import java.io.File
 
 fun main(args: Array<String>) {
-    if (args.size != 3){
-        print("XQL interpreter" +
-                "\nUsage:" +
-                "\n\t xql <XQL file> <XML file> <Output XML file>")
-        return ;
+    if (args.size != 3) {
+        print(
+            "XQL Interpreter\n" +
+                    "Usage:\n" +
+                    "\txql <XQL file> <Input XML file> <Output XML file>"
+        )
+        return;
     }
-    val xql = XQLParser(
+    XQLParser(
         CommonTokenStream(
             XQLLexer(
                 CharStreams.fromString(File(args[0]).readText().trimIndent())
             )
         )
-    ).document().toAst(listOf(args[1], args[2]))
-    xql.run()
+    ).document().toAst(listOf(args[1], args[2])).run()
 }
